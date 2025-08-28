b8d93b46f7150c6697e7cbe0824f91295b918a3e
Ryuji Eguchi <jp.ryuji@gmail.com>
2025-08-28 11:59:21 +1000

    add directive to gen mock to domain/repository

--- internal/domain/repository/company.go
+++ internal/domain/repository/company.go
@@ -6,6 +6,7 @@ import (
 	"github.com/jp-ryuji/go-sample/internal/domain/model"
 )
 
+//go:generate go run go.uber.org/mock/mockgen -source=$GOFILE -destination=mock/$GOFILE -package=mock_repository
 type CompanyRepository interface {
 	Create(ctx context.Context, company *model.Company) error
 	GetByID(ctx context.Context, id string) (*model.Company, error)
